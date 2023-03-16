96303cfb97b11b9bb42ec41f008872472573275e
adesh <adeshbagale123@gmail.com>
2023-03-16 15:24:18 +0530

    aded

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "ec2_type"{
+    type = string
+}
+
+variable "ami_id" {
+    type = string
+  
+}
+
+variable "key_name" {
+    type = string
+    default = "my_pem"
+}
+
+variable "key_path" {
+    type = string
+    default = "C:\Users\Intel\Desktop\Terraform"
+}
